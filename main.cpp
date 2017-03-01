2548b60702cc64a582b6a7e831912e9b13a971a9
tulup <tulup@ascon.ru>
2017-03-01 16:09:05 +0300

    meta::string::length

--- main.cpp
+++ main.cpp
@@ -23,6 +23,17 @@ struct __get_str<T, string_t< Xs... >, I, true >
    using type = typename __get_str<T, string_t<Xs...,T{}.data[I]>, I + 1, T{}.data[I] != '\0' >::type;
 };
 
+
+template <typename T>
+struct __length;
+
+template < template < uint8_t ... > class T, uint8_t ... Xs >
+struct __length< T< Xs... > >
+{
+  constexpr static auto value = sizeof...(Xs);
+};
+
+
 namespace io{
 template < uint8_t ... Xs >
 std::ostream & operator<<( std::ostream & os, meta::string::string_t< Xs ... > )
@@ -38,13 +49,19 @@ using details::io::operator<<;
 template < typename F >
 constexpr auto to_constexpr_string( F && f )
 {
-  return meta::string::get_str_t< decltype(f()) >{};
+  return get_str_t< decltype(f()) >{};
 }
+
+template< typename T >
+constexpr auto length( T && )
+{
+  return details::__length<T>::value;
+}
+
 }
 
 #define CONST_STRING(str) []{ struct { const char * data = str; } _; return _; }
 #define TO_CONST_STRING(str) meta::string::to_constexpr_string( CONST_STRING(str) )
-
 
 int main()
 {
@@ -55,13 +72,16 @@ int main()
   static_assert( sizeof data == foo() );
   std::cout << sizeof data << "\n";
 
-  {
-    using result = meta::string::get_str_t< decltype(foo_str()) >;
-    std::cout << result{};
-  }
+  // {
+  //   using result = meta::string::get_str_t< decltype(foo_str()) >;
+  //   std::cout << result{};
+  // }
 
   {
-    std::cout << TO_CONST_STRING("meta string here");
+    uint8_t data[meta::string::length(TO_CONST_STRING("123")) ] {};
+    static_assert( sizeof data == 4 );
+
+    std::cout << TO_CONST_STRING("constexpr string") << "\n";
   }
 
 }
