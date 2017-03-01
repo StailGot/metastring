3be328a09c2ef1125139ea931f962d93416e8a4a
tulup <tulup@ascon.ru>
2017-03-01 15:18:56 +0300

    cleanup

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <array>
 #include <iostream>
+#include <type_traits>
 
 template < class T >
 constexpr auto V = 42; 
@@ -11,38 +12,33 @@ struct string_t{};
 
 namespace details{
 template <typename T, typename R, size_t I, bool>
-struct get_str;
+struct __get_str;
 
 template <typename T, uint8_t ...Xs, size_t I>
-struct get_str<T, string_t< Xs... >, I, false > { using type = string_t<Xs...>; };
+struct __get_str<T, string_t< Xs... >, I, false > { using type = string_t<Xs...>; };
 
 template <typename T, uint8_t ...Xs, size_t I>
-struct get_str<T, string_t< Xs... >, I, true >
+struct __get_str<T, string_t< Xs... >, I, true >
 {
-   using type = typename get_str<T, string_t<Xs...,T{}.data[I]>, I + 1, T{}.data[I] != '\0' >::type;
+   using type = typename __get_str<T, string_t<Xs...,T{}.data[I]>, I + 1, T{}.data[I] != '\0' >::type;
 };
 
-namespace io
+namespace io{
+template < uint8_t ... Xs >
+std::ostream & operator<<( std::ostream & os, meta::string::string_t< Xs ... > )
 {
-  template < uint8_t ... Xs >
-  std::ostream & operator<<( std::ostream & os, meta::string::string_t< Xs ... > )
-  {
-    return [&]{for( auto i : {Xs ...} ) os << i;}(), os;
-  }
-}
-}
+  return [&]{for( auto i : {Xs ...} ) os << i;}(), os;
+}}}
 
 template <typename T>
-using get_str_t = typename details::get_str< T, string_t<>, 0, true>::type;
+using get_str_t = typename details::__get_str< T, string_t<>, 0, true>::type;
 using details::io::operator<<;
 
 }
 
-
-
 int main()
 {
-  constexpr auto foo_str = []{ struct _ { const char * data = "wtf is that"; }; return _{}; };
+  constexpr auto foo_str = []{ struct _ { const char * data = "wtf is going here?"; }; return _{}; };
   constexpr auto foo = []{ return 42; };
 
   uint8_t data[foo()] = {};
